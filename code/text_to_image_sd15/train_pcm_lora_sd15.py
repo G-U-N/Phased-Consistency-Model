b65a1865aa0bb564a68eb01f3e24665c7498c649
Fu-Yun Wang <1697256461@qq.com>
2024-07-14 17:53:51 +0800

    Update train_pcm_lora_sd15.py

--- code/text_to_image_sd15/train_pcm_lora_sd15.py
+++ code/text_to_image_sd15/train_pcm_lora_sd15.py
@@ -132,6 +132,8 @@ def log_validation(
             beta_end=0.012,
             beta_schedule="scaled_linear",
             timestep_spacing="trailing",
+            clip_sample = False, # important. DDIM will apply True as default which cause inference degradation.
+            set_alpha_to_one = False,
         ),  # DDIM should just work well. See our discussion on parameterization in the paper.
         revision=args.revision,
         torch_dtype=weight_dtype,
